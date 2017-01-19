f2a96107bad0170846f12ee9005127d2904ea563
Пользователь <>
2017-01-19 18:17:13 +0500

    19.01 evening

--- markup/components/header/header.js
+++ markup/components/header/header.js
@@ -1,5 +1,11 @@
-$(document).on('click','.header-search__icon',function(e) {
-	e.preventDefault();
+$(document).on('click','.header-search__icon',function() {
+	$(this).hide();
 	$('.header-search__wrapper').toggleClass('_active');
 	$('.header-search__form .input-form').toggle(300);
-})
+	$('.overlay').toggle();
+});
+
+$(document).on('click','.overlay',function() {
+	$('.header-search__form .input-form').toggle();
+	$('.header-search__icon').toggle();
+});
--- markup/components/list-sub-categories/list-sub-categories.js
+++ markup/components/list-sub-categories/list-sub-categories.js
@@ -1,3 +1,9 @@
 $(document).on('click','.list-sub-categories__mobile',function() {
+	$(this).toggleClass('_active');
+	if($(this).hasClass('_active')) {
+		$(this).html('Скрыть');
+	} else {
+		$(this).html('Подкатегории');
+	}
 	$('.list-sub-categories__list').slideToggle(300);
 })
