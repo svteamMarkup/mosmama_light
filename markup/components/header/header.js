308ab56568fb2de8c7711bc8aac267431abcd4a4
Пользователь <>
2017-01-20 11:53:55 +0500

    верстка выполнена

--- markup/components/header/header.js
+++ markup/components/header/header.js
@@ -2,10 +2,11 @@ $(document).on('click','.header-search__icon',function() {
 	$(this).hide();
 	$('.header-search__wrapper').toggleClass('_active');
 	$('.header-search__form .input-form').toggle(300);
-	$('.overlay').toggle();
-});
+	$('.overlay').show();
+});	
 
 $(document).on('click','.overlay',function() {
 	$('.header-search__form .input-form').toggle();
 	$('.header-search__icon').toggle();
+	$(this).hide();
 });
--- markup/components/menu/menu.js
+++ markup/components/menu/menu.js
@@ -1,3 +1,4 @@
 $(document).on('click','.menu__mobile',function() {
+	$(this).toggleClass('_active');
 	$('.header .menu__list').slideToggle(300);
 })
