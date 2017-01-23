fa3f3711ada71e57acb62e48c3c51f5f399805ff
Пользователь <>
2017-01-23 16:09:32 +0500

    23.01 fixed bugs4

--- markup/components/header/header.js
+++ markup/components/header/header.js
@@ -2,10 +2,10 @@ $(document).on('click','.header-search__icon',function() {
 	$(this).hide();
 	$('.header-search__wrapper').toggleClass('_active');
 	$('.header-search__form .input-form').toggle(300);
-	$('.overlay').show();
+	$('.overlay').removeClass('_menu').show();
 });	
 
-$(document).on('click','.overlay',function() {
+$(document).on('click','.overlay:not(._menu)',function() {
 	$('.header-search__form .input-form').toggle();
 	$('.header-search__icon').toggle();
 	$(this).hide();
--- markup/components/menu/menu.js
+++ markup/components/menu/menu.js
@@ -1,4 +1,13 @@
 $(document).on('click','.menu__mobile',function() {
 	$(this).toggleClass('_active');
 	$('.header .menu__list').slideToggle(300);
+	$('.overlay').addClass('_menu').show();
 })
+
+$(document).on('click','.overlay._menu',function() {
+	$('.header .menu__list').slideToggle(300);
+	$('.header-search__form .input-form').hide();
+	//$('.header-search__icon').toggle();
+	$('.menu__mobile').removeClass('_active');
+	$(this).hide();
+});
